2b6f82e0608ee357549dc50389145b2f3ef15d5a
maxy1991991 <myang1991991@gmail.com>
2026-06-10 17:44:11 -0500

    pokemon widget moved

--- src/App.tsx
+++ src/App.tsx
@@ -43,22 +43,21 @@ function AppContent() {
   
   return (
     <>
-
-      <nav>
-        <Pokemonbutton/>
-        <Link to="/">Home</Link> | <Link to="/aboutme">About Me</Link> | <Link to="/experience">Experience</Link> | <Link to='/goals'>Goals</Link> 
-      </nav>  
-      <div key={location.pathname} className="page-transition">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/aboutme" element={<Aboutme/>} />
-          <Route path="/goals" element={<Goals/>}/>
-          <Route path="/experience" element={<Experience/>}/>
-        </Routes>
-      </div>
-      <Chatwidget/>
+    <nav>
+        <Link to="/">Home</Link> | <Link to="/aboutme">About Me</Link> | <Link to="/experience">Experience</Link> | <Link to='/goals'>Goals</Link>
+    </nav>
+    <div key={location.pathname} className="page-transition">
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<Aboutme/>} />
+        <Route path="/goals" element={<Goals/>}/>
+        <Route path="/experience" element={<Experience/>}/>
+    </Routes>
+    </div>
+    <Pokemonbutton/>
+    <Chatwidget/>
     </>
-  )
+    )
 }
 
 const queryClient = new QueryClient()
